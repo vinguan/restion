1065508c3e68f398333dd93f1c38c9b8a9d17e73
Vinicius Henrique Gualberto da Silva <vinygsilva@hotmail.com>
2014-12-06 22:08:38 -0200

    IJsonSerialiazer

---
+++ Restion/Interfaces/IJsonSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restion.Interfaces
+{
+    public interface IJsonSerializer<T>
+    {
+        void Serialize(T obj);
+        T Deserialize();
+    }
+}
